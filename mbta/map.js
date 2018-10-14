d182d4a5a41ffcc020698c6dd3ee928b5129144f
Josh Simani <jsimani@gmail.com>
2018-10-14 00:48:15 -0400

    Added first marker

--- mbta/map.js
+++ mbta/map.js
@@ -3,4 +3,10 @@ function createMap() {
         center: {lat: 42.352271, lng: -71.05524200000001},
         zoom: 14,
         });
+
+    var marker = new google.maps.Marker({
+          position: {lat: 42.352271, lng: -71.05524200000001},
+          map: map,
+          title: 'South Station'
+        });
 }
