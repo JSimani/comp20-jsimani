3640050d80c4029c83e5371e338876ed5f93c9ac
Josh Simani <jsimani@gmail.com>
2018-11-01 12:17:50 -0400

    Created index.js

---
+++ node-js-getting-started/index.js
@@ -0,0 +1,15 @@
+// Use Express
+var express = require('express');
+var app = express();
+
+app.post("/submit", function(request, response) {
+    response.send([]);
+});
+
+app.get("/scores.json", function(request, response) {
+    response.send([]);
+});
+
+app.get("/", function(request, response) {
+    response.send("Go away!!!");
+});
